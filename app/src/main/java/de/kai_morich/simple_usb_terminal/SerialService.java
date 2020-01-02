78407e584dcaeb4150a061c47c232afb2380682e
kai-morich <mail@kai-morich.de>
2020-01-02 18:48:36 +0100

    migrate to AndroidX libraries

--- app/src/main/java/de/kai_morich/simple_usb_terminal/SerialService.java
+++ app/src/main/java/de/kai_morich/simple_usb_terminal/SerialService.java
@@ -12,8 +12,8 @@ import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Looper;
-import android.support.annotation.Nullable;
-import android.support.v4.app.NotificationCompat;
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 
 import java.util.LinkedList;
 import java.util.Queue;
