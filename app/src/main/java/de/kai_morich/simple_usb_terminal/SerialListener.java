6a1caf2007e208d53138ed7dfa3fab8211c82328
RumitPatel <rumit.patel5@gmail.com>
2023-11-01 17:15:21 +0530

    1. Code formatted as standard android practise. 2. Spelling mistake solved. 3. Small code cleanup in comments.

--- app/src/main/java/de/kai_morich/simple_usb_terminal/SerialListener.java
+++ app/src/main/java/de/kai_morich/simple_usb_terminal/SerialListener.java
@@ -3,9 +3,9 @@ package de.kai_morich.simple_usb_terminal;
 import java.util.ArrayDeque;
 
 interface SerialListener {
-    void onSerialConnect      ();
-    void onSerialConnectError (Exception e);
-    void onSerialRead         (byte[] data);                // socket -> service
-    void onSerialRead         (ArrayDeque<byte[]> datas);   // service -> UI thread
-    void onSerialIoError      (Exception e);
+    void onSerialConnect();
+    void onSerialConnectError(Exception e);
+    void onSerialRead(byte[] data); // socket -> service
+    void onSerialRead(ArrayDeque<byte[]> datas); // service -> UI thread
+    void onSerialIoError(Exception e);
 }
